33087aaa0c4cd41c462c3bcc2a6130d59a3965cf
akhilmondal <akhilmandalfamily@gmail.com>
2023-04-03 10:22:23 +0530

    Added: The null exception is handled to return happy as output.

--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -10,11 +10,13 @@ public class MoodAnalyserTest {
     public void AnalyseMoodMethodWillReturnSad() {
         MoodAnalyserProblem moodAnalyserProblem1 = new MoodAnalyserProblem("i am in sad mood");
         Assert.assertEquals(true,moodAnalyserProblem1.analyseMood());
+        System.out.println("The user is sad: ");
     }
     //bellow test case will pass when result will be fasle or not sad.
     @Test
     public void AnalyseMoodMethodWillReturnHappy() {
-        MoodAnalyserProblem moodAnalyserProblem2 = new MoodAnalyserProblem("i am in any mood");
+        MoodAnalyserProblem moodAnalyserProblem2 = new MoodAnalyserProblem(null);
         Assert.assertEquals(false,moodAnalyserProblem2.analyseMood());
+        System.out.println("The user is happy: ");
     }
 }
